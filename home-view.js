47793327e00d081609706be7abee71a12c99fd31
Katie Chai <kchai@unc.edu>
2024-04-28 12:16:45 -0400

    styling more

--- home-view.js
+++ home-view.js
@@ -4,8 +4,17 @@ export class HomeView {
     }
     
     render(render_div) {
+        // Title
+        let title_div = document.createElement('div');
+        title_div.classList.add('title');
+        let break1 = document.createElement('br');
+
         let title = document.createElement('h1');
-        title.textContent = "Allergy Forecast";
-        render_div.append(title);
+        title.textContent = "Welcome";
+    
+        title_div.append(break1);
+        title_div.append(title);
+        render_div.append(title_div);
+
     }
 }
