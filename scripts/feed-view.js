d3501506c257a8895bde932d7e6e65b3798a869b
Katie Chai <kchai@unc.edu>
2024-04-28 19:49:51 -0400

    feed

--- scripts/feed-view.js
+++ scripts/feed-view.js
@@ -13,18 +13,41 @@ export class FeedView {
         header_div.append(header);
         render_div.append(header_div);
 
-        // LABEL (for input)
+        // Label (for input)
         let input_label = document.createElement('label');
-        input_label.textContent = "Post your comment!";
+        input_label.textContent = "Write your comment!";
         input_label.setAttribute('for', 'input');
         
-        // INPUT (for name)
+        // Input (for name)
         let input = document.createElement('input');
         input.setAttribute('type', 'text');
         input.setAttribute('id', 'input')
 
-        
+        // Submit
+        let submit = document.createElement('button');
+        submit.textContent = 'Post';
+        submit.addEventListener('click', (event) => {
+            let comment_text = input.value.trim();
+            showComments(comment_text, render_div);
+        });
 
-        
+        let comment_div = document.createElement('div');
+        comment_div.classList.add('comment-holder');
+
+        render_div.append(input_label);
+        render_div.append(document.createElement('br'));
+        render_div.append(input);
+        render_div.append(document.createElement('br'));
+        render_div.append(submit);
+        render_div.append(comment_div);
+    }
+
+    showComments(comment_text, render_div, comment_div) {
+        let comment = document.createElement('p');
+        comment.textContent = comment_text;
+        comment.classList.add('comment');
+
+        comment_div.add(comment);
+        render_div.add(comment_div);
     }
 }
