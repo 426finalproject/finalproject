9dfa80aab5751142f11b0bef23530b831de142c1
Madhumitha Gopinath <madhu26@ad.unc.edu>
2024-04-28 18:47:46 -0400

    comment class

--- step.mjs
+++ step.mjs
@@ -1,30 +1,21 @@
-
 import {db} from './db.mjs';
-export class Step {
+export class Comment {
 
     #id
-    #seq_no
-    #instruction
-    #ingredients
+    #name
+    #comment
 
-    constructor (id, seq_no, instruction, ingredients) {
+    static #nextId=1;
+    constructor (id, name, comment) {
         this.#id = id;
-        this.#seq_no = seq_no;
-        this.#instruction = instruction;
-        this.#ingredients = [...ingredients];
+        this.#name = name;
+        this.#comment = comment;
     }
 
     static async create(data) {
-        // Only used by Recipe which prevalidates the data.
-
         try {
-            let db_result = await db.run('insert into steps values (NULL, ?, ?, ?)', data.seq_no, data.instruction, data.recipe_id);
-            let step_id = db_result.lastID;
-            for (let i=0; i<data.ingredients.length; i++) {
-                let iid = data.ingredients[i];
-                let db_result = await db.run('insert into ingredient_step values (?, ?)', iid, step_id);
-            }
-            let step = new Step(db_result.lastID, data.seq_no, data.instruction, data.ingredients);
+            let db_result = await db.run('insert into comments values (NULL, ?, ?)', data.name, data.comment);
+            let step = new Comment(Comment.#nextId++, data.name, data.comment);
             return step;    
         } catch (e) {
             return null;
@@ -33,12 +24,12 @@ export class Step {
 
     static async findByID(id) {
         try {
-            let row = await db.get('select * from steps where id = ?', id);
+            let row = await db.get('select * from comments where id = ?', id);
             if (!row) {
                 return null;
             } else {
-                let step_ing_rows = await db.all('select ingredient_id from ingredient_step where step_id = ?', id);
-                let ingredient_ids = step_ing_rows.map(sir => sir.ingredient_id);
+                let step_ing_rows = await db.all('select name,comment from comments where id = ?', id);
+                let ingredient_ids = step_ing_rows.map(sir => sir.ingredient_id);                         // ???
                 return new Step(row.id, row.seq_no, row.instruction, ingredient_ids);
             }
         } catch (e) {
@@ -48,14 +39,20 @@ export class Step {
 
     json() {
         return {
-            id: this.#id,
-            seq_no: this.#seq_no,
-            instruction: this.#instruction,
-            ingredients: this.getIngredientIDs()
+            id: this.getID(),
+            name: this.getName(),
+            comment: this.getComment(),
         }
     }
 
-    getIngredientIDs() {
-        return [...this.#ingredients];
+
+    getID(){
+        return this.#id;   
+    }
+    getName(){
+        return this.#name;      
+    }
+    getComment() {
+        return this.#comment;
     }
 }
