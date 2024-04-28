ba3493d9341e8d1a2f8bbf713182042e15ea9a0c
Katie Chai <kchai@unc.edu>
2024-04-28 11:49:30 -0400

    more updates for view

--- home-view.js
+++ home-view.js
@@ -1,11 +1,11 @@
 export class HomeView {
+
+    constructor() {
+    }
     
     render(render_div) {
-        let my_title = document.createElement('h1');
-        let heart_animation = document.createElement('img');
-        heart_animation.setAttribute('src', 'images/sparkling-heart.gif')
-        heart_animation.style.width = '35px';
-        my_title.textContent = "Game of ";
-        my_title.append(heart_animation)
+        let title = document.createElement('h1');
+        title.textContent = "Allergy Forecast";
+        render_div.append(title);
     }
 }
--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-import {HomesView} from "./home-view.js";
+import {HomeView} from "./home-view.js";
 
 let view = new HomeView();
 
