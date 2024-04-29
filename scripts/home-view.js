4403119c147289b847959114a631669121ad0da5
Katie Chai <kchai@unc.edu>
2024-04-29 16:33:17 -0400

    async function attempt

--- scripts/home-view.js
+++ scripts/home-view.js
@@ -3,8 +3,12 @@ export class HomeView {
         let body = document.body
 
         // Title
-        let text = `Welcome!<br>Forcast your Allergies.`;
-        this.createTitle(render_div, text);
+        let title_div = document.createElement('div');
+        title_div.classList.add('title');
+        let title = document.createElement('h1');
+        title.innerHTML = `Welcome!<br>Forcast your Allergies.`;
+        title_div.append(title);
+        render_div.append(title_div);
 
         // Button
         let button_div = document.createElement('div');
@@ -12,43 +16,38 @@ export class HomeView {
         let button5 = document.createElement('button');
         button5.setAttribute('id', 'button5');
         button5.textContent= 'Forecast';
-        button5.addEventListener('click', () => {
+
+        // Event Listener
+        button5.addEventListener('click', async () => {
             // Change Background
             body.style.backgroundImage = 'url(/assets/forecast-screen.png)';
-            // Hide
+            // Hiding
             title_div.style.display = 'none';
             title.style.display = 'none';
             button5.style.display = 'none';
             // Audio
             let audio = new Audio('assets/marys-theme.mp3');
             audio.play();
-            // let forecast_data = await this.getForecasts();
-            this.show5DayForecast(render_div);
+            // API
+            let forecast_data = await this.getForecasts();
+            this.show5DayForecast(render_div, forecast_data);
         });
 
         button_div.append(button5);
         render_div.append(button_div);
     }
 
-    createTitle(render_div, text) {
-        let title_div = document.createElement('div');
-        title_div.classList.add('title');
-        let title = document.createElement('h1');
-        title.innerHTML = text;
-        title_div.append(title);
-        render_div.append(title_div);
+    // Async function to call API
+    async getForecasts() {
+        let fetch_result = await fetch('http://localhost:3000/forecast');  // send HTTP GEt request to /forecast endpoint
+        if (!fetch_result.ok) {
+            console.log("Failed to getForecasts!");
+        }
+        let result_json = await fetch_result.json();
+        return result_json;
     }
 
-    createHeader(render_div, text) {
-        let header_div = document.createElement('div');
-        header_div.classList.add('header');
-        let header = document.createElement('h1');
-        header.textContent = text;
-        header_div.append(header);
-        render_div.append(header_div);
-    }
-
-    show5DayForecast(render_div) {
+    show5DayForecast(render_div, forecast_data) {
         // Header
         let text = "5-Day Pollen Forecast";
         this.createHeader(render_div, text);
@@ -58,19 +57,20 @@ export class HomeView {
         forecast_div.classList.add('forecast');
 
         // Forecasts
-        for (let i=0; i < 5; i++) {
+
+        forecast_data.forEach((forcast, i) => {
             let day = document.createElement('div');
             day.classList.add('day');
-
             let day_label = document.createElement('div');
             day_label.classList.add('day-label');
 
             // Data
-            let label = "Monday";
-            let value = "1";
-            let category = "Very Low";
-            let indexDescription = "People with very high allergy to pollen are likely to experience symptoms";
+            let label = forecast.day;
+            let value = forecast.index;
+            let category = forecast.category;
+            let indexDescription = forecast.indexDescription;
             
+
             day_label.innerHTML = `
                 Day: ${label}
                 <br>
@@ -95,11 +95,10 @@ export class HomeView {
                 }
                 this.showMore(render_div);
             });
-
             day.append(day_label);
             day.append(button1);
             forecast_div.append(day);
-        }
+        });
         render_div.append(forecast_div);
     }
 
@@ -139,5 +138,14 @@ export class HomeView {
         day.append(day_label);
         forecast_div.append(day);
         render_div.append(forecast_div);
+    }
+
+    createHeader(render_div, text) {
+        let header_div = document.createElement('div');
+        header_div.classList.add('header');
+        let header = document.createElement('h1');
+        header.textContent = text;
+        header_div.append(header);
+        render_div.append(header_div);
     }
 }
