d78eaf1a9d87548b6925d96fc6ea453d3bb6a48e
Madhumitha Gopinath <madhu26@ad.unc.edu>
2024-04-28 20:25:26 -0400

    comment file

--- comment.mjs
+++ comment.mjs
@@ -1,88 +1,58 @@
 import {db} from './db.mjs';
-export class PollenData {
+export class Comment {
 
     #id
-    #location
-    #value
-    #category
+    #name
+    #comment
 
-    
-
-    constructor (id, name) {
+    static #nextId=1;
+    constructor (id, name, comment) {
         this.#id = id;
         this.#name = name;
+        this.#comment = comment;
     }
 
     static async create(data) {
-        if ((data !== undefined) && (data instanceof Object) 
-        && (data.name !== undefined) 
-        && (typeof data.name == 'string')) {
-
-            try {
-                let db_result = await db.run('insert into ingredients values (NULL, ?)', data.name);
-                let ing = new Ingredient(db_result.lastID, data.name);
-                return ing;
-            } catch (e) {
-                return null;
-            }
-        }
-        return null;
-    }
-
-    static async getAllIDs() {
         try {
-            let rows = await db.all('select id from ingredients');
-            return rows.map(r => r.id);
+            let db_result = await db.run('insert into comments values (NULL, ?, ?)', data.name, data.comment);
+            let step = new Comment(Comment.#nextId++, data.name, data.comment);
+            return step;    
         } catch (e) {
-            return [];
+            return null;
         }
     }
 
     static async findByID(id) {
         try {
-            let row = await db.get('select * from ingredients where id = ?', id);
+            let row = await db.get('select * from comments where id = ?', id);
             if (!row) {
                 return null;
             } else {
-                return new Ingredient(row.id, row.name);
+                let step_ing_rows = await db.all('select name,comment from comments where id = ?', id);
+                let ingredient_ids = step_ing_rows.map(sir => sir.ingredient_id);                         // ???
+                return new Step(row.id, row.seq_no, row.instruction, ingredient_ids);
             }
         } catch (e) {
             return null;
         }
     }
 
-    static async deleteIngredientByID(id) {
-        try {
-            // First see if it is still in use. If so, return false.
-            let ing_step_count = await db.get('select count(*) as count from ingredient_step where ingredient_id = ?', id);
-            if (ing_step_count.count != 0) {
-                return false;
-            }
-            await db.run('delete from ingredients where id = ?', id);
-            return true;
-        } catch (e) {
-            return false;
-        }
-    }
-
     json() {
         return {
-            id: this.#id,
-            name: this.#name
+            id: this.getID(),
+            name: this.getName(),
+            comment: this.getComment(),
         }
     }
 
-    getID() {
-        return this.#id;
-    }
 
-    async setName(new_name) {
-        try {
-            await db.run('update ingredients set name = ? where id = ?', this.#name, this.#id);
-            this.#name = new_name;
-            return true;
-        } catch (e) {
-            return false;
-        }
+    getID(){
+        return this.#id;   
+    }
+    getName(){
+        return this.#name;      
+    }
+    getComment() {
+        return this.#comment;
     }
 }
