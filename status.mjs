cc0b4836de424d2f1c16a881866f9b5eb3a2246c
Madhumitha Gopinath <madhu26@ad.unc.edu>
2024-04-30 16:46:15 -0400

    status update :)

--- status.mjs
+++ status.mjs
@@ -1,6 +1,7 @@
 import {db} from './db.mjs';
+
 export class Status {
-    
+
     #id
     #text
 
@@ -9,8 +10,52 @@ export class Status {
         this.#text = text;
     }
 
-    static setText(){
-        
+    static async setText(data){
+        try {
+            let db_result = await db.run('insert into statuses values (?, ?)', data.id, data.text);
+            if (!db_result) {
+                return null;
+            }
+
+            return {
+                id: data.id,
+                text: data.text
+            }
+        } catch (e) {
+            return null;
+        }
     }
+
+    static async updateText(data){
+        try {
+            await db.run('update statuses set text = ? where id = ?', data.text, data.id);
+        } 
+        catch (e) {
+            return false;
+        }
+        return true;
+
+    }
+
+    static async getText(id){
+        try{
+            let status = await db.get('select text from statuses where id = ?', id);
+
+            if (!status) {
+                return null;
+            }
+
+            return {
+                id: data.id,
+                text: status.text
+            }
+
+        } catch(e){
+            return null;
+        }
+
+    }
+
+
 
 }
