ff3a8e46bd02f6d7ae5ff2fe9448f81b13c7d09b
Madhumitha Gopinath <madhu26@ad.unc.edu>
2024-04-30 16:06:08 -0400

    create status.mjs

---
+++ status.mjs
@@ -0,0 +1,16 @@
+import {db} from './db.mjs';
+export class Status {
+    
+    #id
+    #text
+
+    constructor (id, text) {
+        this.#id = id;
+        this.#text = text;
+    }
+
+    static setText(){
+        
+    }
+
+}
